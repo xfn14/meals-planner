c185e5185d9a7161b2034e6cce1b7539bb32c7a8
André Vaz <andre.vaz1411@gmail.com>
2025-06-17 15:48:34 +0100

    Fix eslint.config + eslint-compat error

--- eslint.config.js
+++ eslint.config.js
@@ -7,55 +7,57 @@ const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
-export default tseslint.config(
-  {
-    ignores: [".next"],
-  },
+export default [
   ...compat.extends("next/core-web-vitals"),
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    plugins: {
-      drizzle,
+  ...tseslint.config(
+    {
+      ignores: [".next"],
     },
-    extends: [
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
-    rules: {
-      "@typescript-eslint/array-type": "off",
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/consistent-type-imports": [
-        "warn",
-        { prefer: "type-imports", fixStyle: "inline-type-imports" },
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      plugins: {
+        drizzle,
+      },
+      extends: [
+        ...tseslint.configs.recommended,
+        ...tseslint.configs.recommendedTypeChecked,
+        ...tseslint.configs.stylisticTypeChecked,
       ],
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        { checksVoidReturn: { attributes: false } },
-      ],
-      "drizzle/enforce-delete-with-where": [
-        "error",
-        { drizzleObjectName: ["db", "ctx.db"] },
-      ],
-      "drizzle/enforce-update-with-where": [
-        "error",
-        { drizzleObjectName: ["db", "ctx.db"] },
-      ],
-    },
-  },
-  {
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
+      rules: {
+        "@typescript-eslint/array-type": "off",
+        "@typescript-eslint/consistent-type-definitions": "off",
+        "@typescript-eslint/consistent-type-imports": [
+          "warn",
+          { prefer: "type-imports", fixStyle: "inline-type-imports" },
+        ],
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          { argsIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/no-misused-promises": [
+          "error",
+          { checksVoidReturn: { attributes: false } },
+        ],
+        "drizzle/enforce-delete-with-where": [
+          "error",
+          { drizzleObjectName: ["db", "ctx.db"] },
+        ],
+        "drizzle/enforce-update-with-where": [
+          "error",
+          { drizzleObjectName: ["db", "ctx.db"] },
+        ],
       },
     },
-  },
-);
+    {
+      linterOptions: {
+        reportUnusedDisableDirectives: true,
+      },
+      languageOptions: {
+        parserOptions: {
+          projectService: true,
+        },
+      },
+    },
+  ),
+];
